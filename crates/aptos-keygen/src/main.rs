85cf3ee3959e0fa1c8e802c6b9bc98398706b8d4
zhiqiangxu <652732310@qq.com>
2022-10-31 17:56:18 +0800

    mod aptos-keygen

--- crates/aptos-keygen/src/main.rs
+++ crates/aptos-keygen/src/main.rs
@@ -4,24 +4,44 @@
 use aptos_crypto::ValidCryptoMaterialStringExt;
 use aptos_keygen::KeyGen;
 use aptos_types::transaction::authenticator::AuthenticationKey;
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
+#[derive(Parser, Default)]
+struct Arguments {
+    #[clap(short, long)]
+    pub n: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Account {
+    pub addr: String,
+    pub pk: String,
+}
 fn main() {
     let mut keygen = KeyGen::from_os_rng();
-    let (privkey, pubkey) = keygen.generate_ed25519_keypair();
+    let args = Arguments::parse();
 
-    println!("Private Key:");
-    println!("{}", privkey.to_encoded_string().unwrap());
+    let mut results = vec![];
+    let mut m = HashMap::new();
+    for _ in 0..args.n {
+        let (privkey, pubkey) = keygen.generate_ed25519_keypair();
 
-    println!();
+        let auth_key = AuthenticationKey::ed25519(&pubkey);
+        let account_addr = auth_key.derived_address();
 
-    let auth_key = AuthenticationKey::ed25519(&pubkey);
-    let account_addr = auth_key.derived_address();
+        if m.contains_key(&account_addr) {
+            println!("oops, duplicate key founded!!!");
+            continue;
+        }
+        m.insert(account_addr, ());
 
-    println!("Auth Key:");
-    println!("{}", auth_key.to_encoded_string().unwrap());
-    println!();
+        results.push(Account {
+            pk: privkey.to_encoded_string().unwrap(),
+            addr: account_addr.to_hex(),
+        })
+    }
 
-    println!("Account Address:");
-    println!("{}", account_addr);
-    println!();
+    println!("{}", serde_json::to_string(&results).unwrap())
 }
